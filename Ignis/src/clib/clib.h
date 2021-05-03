db44053fe352e88aa48355f9d67bd4393c73c99e
oliverjakobs <31634843+oliverjakobs@users.noreply.github.com>
2021-05-03 02:10:23 +0200

    make ignis a lib to be included as submodule

--- Ignis/src/clib/clib.h
+++
@@ -1,7 +0,0 @@
-#ifndef CLIB_H
-#define CLIB_H
-
-#define MAX(a,b) ((a) > (b) ? a : b)
-#define MIN(a,b) ((a) < (b) ? a : b)
-
-#endif /* !CLIB_H */
--- premake5.lua
+++ premake5.lua
@@ -1,51 +1,25 @@
-workspace "Ignis"
-	architecture "x64"
-	startproject "Ignis"
-
-	configurations
-	{
-		"Debug",
-		"Release"
-	}
-
-outputDir = "%{cfg.buildcfg}"
-
-include "Ignis/packages/glfw"
-
 project "Ignis"
-	location "Ignis"
-	kind "ConsoleApp"
+	kind "StaticLib"
 	language "C"
 	cdialect "C99"
-	staticruntime "On"
 	
 	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
 
 	files
 	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.c",
-		--Config
-		"%{prj.name}/config.json",
-		--Resources
-		"%{prj.name}/res/fonts/**.ttf",
-		"%{prj.name}/res/shaders/**.vert",
-		"%{prj.name}/res/shaders/**.frag",
-		"%{prj.name}/res/templates/**.json",
-		"%{prj.name}/res/textures/**.png"
+		"src/**.h",
+		"src/**.c"
 	}
 
 	links
 	{
-		"GLFW",
 		"opengl32"
 	}
 
 	includedirs
 	{
-		"%{prj.name}/src",
-		"%{prj.name}/packages/glfw/include"
+		"src"
 	}
 
 	filter "system:windows"
