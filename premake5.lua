329c6f2d01c80c05335bd8039d8b9718a4daa566
oliverjakobs <31634843+oliverjakobs@users.noreply.github.com>
2021-05-04 01:54:37 +0200

    updated build location

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "Ignis"
 	kind "StaticLib"
 	language "C"
 	
-	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+	targetdir ("build/bin/" .. output_dir .. "/%{prj.name}")
+	objdir ("build/bin-int/" .. output_dir .. "/%{prj.name}")
 
 	files
 	{
