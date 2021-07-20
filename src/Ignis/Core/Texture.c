5132afb6b6dc3aceaf2408a69d994dfb9df0cb43
Oliver Jakobs <oliverjakobs@gmx.net>
2021-07-20 14:33:19 +0200

    abort texture loading if file could not be read

--- src/Ignis/Core/Texture.c
+++ src/Ignis/Core/Texture.c
@@ -51,6 +51,8 @@ int ignisCreateTexture2D(IgnisTexture2D* texture, const char* path, GLuint rows,
 	size_t buffer_len;
 	char* buffer = ignisReadFile(path, &buffer_len);
 
+	if (!buffer) return IGNIS_FAILURE;
+
 	unsigned char* pixels = stbi_load_from_memory(buffer, (int)buffer_len, &texture->width, &texture->height, &bpp, 4);
 
 	ignisFree(buffer);
